9a699dabb2d053bd34c8a852833d363720a602a0
Thomas Omans <thomas.omans@gmail.com>
2014-01-18 03:16:12 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject warden "0.0.1-SNAPSHOT"
+  :description "a web app for supervisor"
+  :url "https://github.com/eggsby/warden"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.6"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler warden.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
