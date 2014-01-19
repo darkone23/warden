45fea727feeec91f79a3da3336229174336d3255
Thomas Omans <thomas.omans@gmail.com>
2014-01-19 02:11:39 -0500

    Hooking up cljs pulse to supervisor api

--- src/cljs/warden/core.cljs
+++ src/cljs/warden/core.cljs
@@ -1,15 +1,30 @@
 (ns warden.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [sablono.core :as html :refer [html] :include-macros true]))
+            [sablono.core :as html :refer [html] :include-macros true]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<! timeout]])
+  (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
-(def state {:name "warden"})
+(def app-state (atom {:route :home
+                      :name "warden"}))
+
+(def wait 5000)
+
+(defn load! [url]
+  (go-loop [response (<! (http/get url))]
+    (let [route (:route @app-state)]
+      (swap! app-state assoc :name (:body response))
+      (<! (timeout wait))
+      (if (= :home route)
+        (recur (<! (http/get url)))))))
 
 (defn app [state]
   (let [{name :name} state
-        greeting (str "Hello " name "!")]
+        greeting (str "Hello " name)]
     (om/component
       (html [:h1 greeting]))))
 
 (defn start []
-  (om/root state app js/document.body))
+  (load! "/api/supervisors")
+  (om/root app-state app js/document.body))
