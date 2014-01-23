cf8bb038435ac4b743f69d01300bd98f2592868f
Thomas Omans <thomas.omans@gmail.com>
2014-01-23 02:20:38 -0500

    more ui love

--- src/cljs/warden/components.cljs
+++ src/cljs/warden/components.cljs
@@ -2,29 +2,33 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [sablono.core :as html :refer (html) :include-macros true]
-            [warden.font-awesome :refer (font-icon)]
-            [warden.pure :refer (responsive-grid menu grid-unit grid-row)]))
+            [warden.helpers :refer (add-class hide-on-phone menu font-icon
+                                    responsive-grid grid-unit grid-row)]))
 
 (declare app process supervisor)
 
 (defn supervisor-err
   "Render the error section for an unreachable supervisor"
   [{:keys [host name port] {err :fault-string} :state}]
-  (let [error (str "Could not connect to " name " at " host ":" port " - " err)]
+  (let [message (str "Could not connect to " name " at " host ":"  port)]
     (grid-row [:section.supervisor.error
-               (font-icon :exclamation-triangle 2)
-               error])))
+               (grid-unit [:span.message message])
+               (grid-unit [:code.err err])
+               (font-icon :exclamation-triangle 2)])))
 
 (defn supervisor-ok [{:keys [host port name processes pid state id version]}]
   (let [public-url (str "http://" host ":" port)
-        supervisord-description (str name " - " host)
-        supervisord-state (str (:statename state) " - " (count processes) " processes")]
+        supervisord-description (str name "@" host)
+        state (:statename state)]
     [:section.supervisor
      (responsive-grid [:header
          (grid-unit 5 6 [:span.description
                           [:a {:href public-url
                                :target "_blank"} supervisord-description]])
-         (grid-unit 1 6 [:span.state supervisord-state])])
+         (hide-on-phone
+         (grid-unit 1 6 [(add-class :span.state state)
+                          [:span.process-count (count processes)]
+                          (font-icon :check)]))])
      (responsive-grid [:ul.processes (map process processes)])]))
 
 (defn supervisor [supervisor]
@@ -32,8 +36,11 @@
     (supervisor-err supervisor)
     (supervisor-ok supervisor)))
 
-(defn process [{:keys [name description]}]
-  (grid-row [:li (str name" - " description)]))
+(defn process [{:keys [name statename description]}]
+  (grid-row [(keyword (str "li.process." statename))
+    (grid-unit [:span.name name])
+    (grid-unit [:span.description description])
+    (grid-unit [:span.state statename])]))
 
 (defn app [state]
   "App as a function of application state"
@@ -43,5 +50,6 @@
       (responsive-grid [:div.main
                    (menu [:header
                 (grid-unit [:h2 name])
-                (grid-unit [:h3.description description])])
+                       (-> [:h3.description description]
+                           grid-unit hide-on-phone)])
              (grid-row [:div.supervisors (map supervisor supervisors)])])))))
---
+++ src/cljs/warden/helpers.cljs
@@ -0,0 +1,46 @@
+(ns warden.helpers)
+
+(defn add-class [el class]
+  "adds a class-name to a sablono html vector"
+  (-> (name el) (str "." class) keyword))
+
+;; helpers for working with font awesome
+;; http://fortawesome.github.io/Font-Awesome/
+
+(defn icon-to-name [icon-name]
+  (str "fa-" (name icon-name)))
+
+(defn font-icon
+  ([icon-key] (font-icon icon-key 1 nil))
+  ([icon-key scale] (font-icon icon-key scale nil))
+  ([icon-key scale content]
+     (let [icon-name (icon-to-name icon-key)
+           scale (str "fa-" scale "x")
+           class (str "fa." icon-name "." scale)]
+       [(add-class :i class) content])))
+
+
+;; helpers for working with purecss
+;; http://purecss.io/
+
+(defn grid [html]
+  (update-in html [0] add-class "pure-g"))
+
+(defn responsive-grid [html]
+  "adds the purecss class-name for a responsive grid"
+  (update-in html [0] add-class "pure-g-r"))
+
+(defn grid-row [html]
+  (update-in html [0] add-class (str "pure-u-1")))
+
+(defn grid-unit
+  "create a grid unit, either an entire row or size x of n"
+  ([html] (update-in html [0] add-class (str "pure-u")))
+
+  ([x n html] (update-in html [0] add-class (str "pure-u-" x "-" n))))
+
+(defn menu [html]
+  (update-in html [0] add-class  "pure-menu.pure-menu-fixed.pure-menu-horizontal"))
+
+(defn hide-on-phone [html]
+  (update-in html [0] add-class "pure-hidden-phone"))
