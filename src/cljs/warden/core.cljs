34b9391702b5eb028e46c0b71d269e46d2690556
Thomas Omans <thomas.omans@gmail.com>
2014-01-24 11:24:19 -0800

    removing unnecessary async logic

--- src/cljs/warden/core.cljs
+++ src/cljs/warden/core.cljs
@@ -1,18 +1,13 @@
 (ns warden.core
   (:require [om.core :as om :include-macros true]
-            [cljs.core.async :refer (<!)]
-            [cljs-http.client :as http]
             [alandipert.storage-atom :refer (local-storage)]
-            [warden.components :refer (app)]
-            [warden.net :refer (parse)])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+            [warden.components :refer (app)]))
 
 (defn ^:export start []
-  (go
-   (let [supervisors (parse (<! (http/get "/api/supervisors")))
-         config (local-storage (atom {:showing #{}}) :config)
-         app-state (atom {:name "warden"
-                          :description "process management"
-                          :supervisors supervisors
-                          :config config})]
-    (om/root app-state app js/document.body))))
+  (let [config (local-storage
+                 (atom {:showing #{}}) :config)
+        app-state (atom {:name "warden"
+                         :description "process management"
+                         :supervisors []
+                         :config config})]
+    (om/root app-state app js/document.body)))
