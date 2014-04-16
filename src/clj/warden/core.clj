e7f959bbff188dbfa3aa5e83c6345738a7bf7b29
Thomas Omans <thomas.omans@gmail.com>
2014-04-16 09:33:01 -0700

    printing out the bind port on startup

--- src/clj/warden/core.clj
+++ src/clj/warden/core.clj
@@ -4,5 +4,7 @@
             [org.httpkit.server :refer [run-server]]))
 
 (defn -main [& args]
-  (run-server app {:port 8080}))
+  (let [port 8080]
+    (println (str "Starting server on http://0.0.0.0:" port))
+    (run-server app {:port port})))
            
