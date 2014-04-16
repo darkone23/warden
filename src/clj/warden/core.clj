1a05acf9235592267612c3660769ee29b4bf955f
Thomas Omans <thomas.omans@gmail.com>
2014-04-16 15:16:26 -0700

    taking port configuration from environment

--- src/clj/warden/core.clj
+++ src/clj/warden/core.clj
@@ -4,7 +4,8 @@
             [org.httpkit.server :refer [run-server]]))
 
 (defn -main [& args]
-  (let [port 8080]
+  (let [env-port (System/getenv "PORT")
+        port (if env-port (Integer/parseInt env-port) 8080)]
     (println (str "Starting server on http://0.0.0.0:" port))
     (run-server app {:port port})))
            
